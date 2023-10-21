be36c61ecd684e84d7bc780203250c14128de390
prajwal <prajwalr.thedevelper@gmail.com>
2023-10-21 22:50:59 +0530

    fix: deployment issue try 7

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,9 +6,8 @@ import useSWR from "swr";
 // import Loading from "@/components/loading";
 
 async function getData() {
-  const origin = new URL(process.env.VERCEL_URL as string).origin;
 
-  const response = await fetch(new Request(`${origin}/api/getMessages`));
+  const response = await fetch(new Request(`/api/getMessages`));
   return await response.json();
 }
 
