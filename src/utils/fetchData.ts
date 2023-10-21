ea025db105746d25f88b5a2a74c67308a98ee9e0
prajwal <prajwalr.thedevelper@gmail.com>
2023-10-21 23:21:25 +0530

    fix: deployment issue try 10

--- src/utils/fetchData.ts
+++ src/utils/fetchData.ts
@@ -2,7 +2,7 @@ export async function getData() {
     const response = await fetch(
       `${process.env.NEXT_PUBLIC_VERCEL_URL || 'http://localhost:3000'}/api/getMessages`
     );
-    console.log(response);
+    if(!response.ok) throw new Error('Error fetching data');
     const data= await response.json();
     return data || null;
   }
