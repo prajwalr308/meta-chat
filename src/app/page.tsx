45eadb263dade3c34645c54c5c09ec03f3708d76
prajwal <prajwalr.thedevelper@gmail.com>
2023-10-21 23:54:12 +0530

    fix: deployment issue try 13

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,20 +4,20 @@ import Image from "next/image";
 import { Message } from "../../typing";
 import useSWR from "swr";
 import { getData } from "@/utils/fetchData";
+import { fetcher } from "@/utils/fetchMessages";
 // import Loading from "@/components/loading";
 
-
-
 export default async function Home() {
-  const data = await getData();
+  
+  const { data, error } = useSWR('/api/getMessages', fetcher);
   console.log(data);
-  const messages: Message[] = data.messages;
+  const messages: Message[] = data || [];
 
   // if (!messages) return <Loading />;
   return (
     <main>
       {/* messge list*/}
-      <MessageList initialMessages={messages|| []} />
+      <MessageList initialMessages={messages || []} />
 
       {/* chat input */}
       <ChatInput />
